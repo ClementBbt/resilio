f556efb91341dbae748527e31e554f2a3d1da517
Clement Bibolet <clementbibolet@gmail.com>
2020-11-07 12:39:11 +0100

    exo 1

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,10 @@
 class Subscription < ApplicationRecord
   belongs_to :user
   has_many :transactions, dependent: :destroy
+
+  def limit_date
+    self.end_date ? self.end_date - 30 : "Pas d'engagement"
+  end
+
 end
+
