fad9d05b343792a46fdc36de85b01b7a54ae7947
Clement Bibolet <clementbibolet@gmail.com>
2020-11-10 19:39:14 +0100

    done

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,10 +1,14 @@
 class Subscription < ApplicationRecord
   belongs_to :user
   has_many :transactions, dependent: :destroy
+  CATEGORIES = ['Streaming', 'Energie', 'Loisirs', 'Telecom', 'Divers']
+
 
   def limit_date
     self.end_date ? self.end_date - 30 : "Pas d'engagement"
   end
 
 end
+
+#faire un before save soit un validate, et appeler une methode qui fait que la sous a bien une category
 
