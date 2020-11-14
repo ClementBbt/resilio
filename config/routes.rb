73a1a9680df91c449beedcf9a836218d00d8f5ba
Clement Bibolet <clementbibolet@gmail.com>
2020-11-14 17:32:17 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'resiliations/index'
-  get 'resiliations/show'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :subscriptions, only: [ :new, :create, :index, :show ] do
     member do
+      get "/resiliation" , to: "subscriptions#resiliation"
       # route pour les transactions d'une subscription
     end
   end
