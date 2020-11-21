c048c91b7a64bb1088c789deb57041115cadaf43
camillesubtil <camillesubtil.pro@gmail.com>
2020-11-21 19:21:08 +0100

    done comme Clément

--- app/javascript/plugins/init_date_toggle.js
+++ app/javascript/plugins/init_date_toggle.js
@@ -4,8 +4,10 @@ const dateToggle = () => {
     button.addEventListener("change", (e) => {
       if (e.target.value === "Oui") {
         document.querySelector("#if-yes").classList.remove("d-none")
+        document.querySelector("#subscription_status").value = "En cours"
       } else {
         document.querySelector("#if-yes").classList.add("d-none")
+        document.querySelector("#subscription_status").value = "sans engagement"
       }
     })
   })
